1709dfbbf6edbd6a24a6702505fd485cd2d7ec19
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-08-31 17:30:19 -0300

    Delete arquiteturag1 directory

--- arquiteturag1/backend/src/routes/ordersRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ordersController = require("../controllers/ordersController");
-
-router.get("/",ordersController.getAllOrders);
-router.post("/",ordersController.createOrder);
-
-module.exports = router;
