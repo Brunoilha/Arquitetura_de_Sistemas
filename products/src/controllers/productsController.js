ae7982eda3e241ba605733611e4ff923a28af543
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-10-15 21:09:38 -0300

    Update productsController.js

--- products/src/controllers/productsController.js
+++ products/src/controllers/productsController.js
@@ -75,10 +75,22 @@ async function updateProduct(req, res) {
 async function deleteProduct(req, res) {
   try {
     const { id } = req.params;
+
+    const product = await prisma.product.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: "Produto não encontrado" });
+    }
+
     await prisma.product.delete({ where: { id: parseInt(id) } });
-    res.status(204).send();
+
+    res.status(200).json({ message: `Produto '${product.name}' deletado com sucesso` });
   } catch (e) {
-    res.status(500).json({ error: "Error deleting product", details: e.message });
+    res
+      .status(500)
+      .json({ error: "Erro ao deletar o produto", details: e.message });
   }
 }
 
@@ -119,3 +131,4 @@ module.exports = {
   deleteProduct,
   updateProductStock,
 };
+
